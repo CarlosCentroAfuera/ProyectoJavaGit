4278abeb3da27284ef5bb167693ff6279663a098
CarlosCentroAfuera <72136781+CarlosCentroAfuera@users.noreply.github.com>
2021-11-23 10:24:05 +0100

    Update Main.java
    Estoy bien Añadido

--- src/es/centroafuera/Main.java
+++ src/es/centroafuera/Main.java
@@ -3,7 +3,8 @@ package es.centroafuera;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hola");
+	System.out.println("Hola");
         System.out.println("Que tal estás?");
+        System.out.println("Bien!");
     }
 }
